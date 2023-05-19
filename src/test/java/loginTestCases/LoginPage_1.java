3ecd1d754148477a1b8b6bbafbfc0b35308391d1
nazneen.rahman <itsnazn33n@gmail.com>
2023-05-19 15:30:10 +0600

    add the login feature using json file, add the priority

--- src/test/java/loginTestCases/LoginPage_1.java
+++ src/test/java/loginTestCases/LoginPage_1.java
@@ -1,5 +1,6 @@
 package loginTestCases;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -14,17 +15,34 @@ public class LoginPage_1 {
 
     @FindBy(id = "login-button")
     WebElement loginButton;
+
+    @FindBy(id = "react-burger-menu-btn")
+    WebElement menuList;
+    @FindBy(id = "logout_sidebar_link")
+    WebElement logoutButton;
+    @FindBy(css= "#login_button_container > div > form > div.error-message-container.error > h3")
+    WebElement errorAuthMessage;
     protected static WebDriver chromeDriver;
-    public LoginPage_1(WebDriver chromeDriver)
-    {
-         this.chromeDriver = chromeDriver;
+
+    public LoginPage_1(WebDriver chromeDriver) {
+        this.chromeDriver = chromeDriver;
         PageFactory.initElements(chromeDriver, this);
     }
-    public void doLogin_1(String username, String password)
-    {
+
+    public void loginWithValidUsers(String username, String password) throws InterruptedException {
         userName.sendKeys(username);
         passwordValue.sendKeys(password);
         loginButton.click();
+        menuList.click();
+        logoutButton.click();
+
+    }
+
+    public String loginWithInvalidPassword(String username, String password) {
+        userName.sendKeys(username);
+        passwordValue.sendKeys(password);
+        loginButton.click();
+        return errorAuthMessage.getText();
     }
 }
 
--- src/test/java/loginTestCases/LoginTestRunner_1.java
+++ src/test/java/loginTestCases/LoginTestRunner_1.java
@@ -2,21 +2,38 @@ package loginTestCases;
 
 import org.json.simple.parser.ParseException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.v85.layertree.model.StickyPositionConstraint;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
 public class LoginTestRunner_1 extends Setup_1 {
-    @Test
-    public void loginToWebsite_1() throws IOException, ParseException, InterruptedException {
+    LoginPage_1 loginPage1;
+    Utils utils;
+    @Test(priority = 1)
+    public void loginWithValidUsers_1() throws IOException, ParseException, InterruptedException {
 
-        LoginPage_1 loginPage_1 = new LoginPage_1(chromeDriver);
-        Utils utils = new Utils();
-        utils.getUsers(0);
+          loginPage1 = new LoginPage_1(chromeDriver);
+          utils = new Utils();
+          utils.getUsers(0);
         {
              chromeDriver.get("https://www.saucedemo.com/");
-             loginPage_1.doLogin_1(utils.getEmail(), utils.getPassword());
-             Thread.sleep(3000);
+             loginPage1.loginWithValidUsers(utils.getEmail(), utils.getPassword());
+             Thread.sleep(2000);
+
+        }
+    }
+    @Test(priority = 2)
+    public void loginWithInvalidPassword() throws IOException, ParseException, InterruptedException {
+
+        loginPage1 = new LoginPage_1(chromeDriver);
+        utils = new Utils();
+        utils.getUsers(1);
+        {
+           String errorMessage =  loginPage1.loginWithInvalidPassword(utils.getEmail(), utils.getPassword());
+           Assert.assertTrue(errorMessage.contains("service"));
+           System.out.println("The error message is:" + errorMessage);
 
         }
     }
